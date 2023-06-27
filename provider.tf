47b05658f23be2463e13abd8aa870a46edecfef0
Gowthaminr <84511384+Gowthaminr@users.noreply.github.com>
2023-06-27 14:20:18 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -9,6 +9,8 @@ terraform {
 
 provider "aws" {
 	region = var.region
-	access_key               = "AKIA5EILTROZ3WC7XSGS"
-        secret_key               = "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"
+	# access_key               = "AKIA5EILTROZ3WC7XSGS"
+        # secret_key               = "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"
+	access_key                 = "AKIA5BMGLEQ56KTRH7NK"
+        secret_key               = "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"
 }
