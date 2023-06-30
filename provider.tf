ad6bb62ca67ecb158e8f295bcb877c37dcdbcf09
Gowthaminr <84511384+Gowthaminr@users.noreply.github.com>
2023-06-30 13:13:01 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -11,6 +11,6 @@ provider "aws" {
 	region = var.region
 	# access_key               = "AKIA5EILTROZ3WC7XSGS"
         # secret_key               = "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"
-	access_key                 = "AKIA5BMGLEQ56KTRH7NK"
-        secret_key                 = "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"
+	access_key                 = "AKIASZDAANNMGMTKAPC5"
+        secret_key                 = "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"
 }
